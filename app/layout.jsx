f81a926fe010e19f5d7816b970649b88592d4ee9
DuyThanh <thanhnd2512@gmail.com>
2023-05-17 08:31:17 +0700

    init project

---
+++ app/layout.jsx
@@ -0,0 +1,17 @@
+import "@styles/styles.css";
+import React from "react";
+
+export const metadata = {
+  title: "bototayninhphuquoc",
+  description: "BÒ TƠ TÂY NINH SÁU TÂM 68",
+};
+
+const layout = ({ children }) => {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+};
+
+export default layout;
